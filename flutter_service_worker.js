e2e6e7cb2baefd2f621cfa5e300f2261acf165c6
lavrov <lavrov@users.noreply.github.com>
2021-07-25 21:17:15 +0000

    deploy: ZaiChat/client-flutter@9e46edb5498fa35f9450d1dbb027f2e34a8bfae6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,15 +4,14 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "6ff87c8b7ef7449a1309458c6095a4ee",
-"index.html": "85257ab18df03a4ec209a57975fc0db4",
-"/": "85257ab18df03a4ec209a57975fc0db4",
+"main.dart.js": "af570aa7634222ec9f7f538896d6d476",
+"index.html": "59a59166f2ccb82d7e17c68dea78e8a5",
+"/": "59a59166f2ccb82d7e17c68dea78e8a5",
 "manifest.json": "6b7b23d36c7b1dad72b8af0b5a3d6e8f",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/NOTICES": "e4e1c0ed5933bc4cdb65b3e58b627d1b",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/NOTICES": "970166c917e56f3ebf9edeed738d9997",
+"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "version.json": "56692561568c255ab2663c5c7d1b941a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
